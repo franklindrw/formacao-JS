c72b2c0446aa8a2614f55f13e6b3f0c6a0826537
franklindrw <franklindrw@gmail.com>
2022-01-21 12:11:42 -0300

    exemplos de Variáveis

---
+++ fundamentos/type-boolean.js
@@ -0,0 +1,14 @@
+/**
+ * true -> verdadeiro
+ * false -> falso
+ */
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+
+console.log(primeiroNumero === segundoNumero);
+
+const txt1 = "Alura";
+const txt2 = "alura";
+
+console.log(txt1 == txt2);
